73cbaaee09ebe9632e351d030b5f2a29f04f07ac
Parth Joshi <parth.00.joshi@gmail.com>
2025-07-07 19:17:40 +0530

    sample website commit

---
+++ webpack configured website/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: {
+		index: './src/index.js',
+		courses: './src/pages/courses.js'
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
+	},
+	devServer: {
+		static: './dist'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(css)$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.s[a|c]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
+				test: /\.(png|jpeg|jpg|svg|gif)$/,
+				type: 'asset/resource'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			chunks: ['index'],
+			inject: true,
+			filename: 'index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/pages/courses.html',
+			chunks: ['courses'],
+			inject: true,
+			filename: 'courses.html'
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, './src/assets/images/*'),
+					to: path.resolve(__dirname, 'dist'),
+					context: 'src'
+				}
+			]
+		})
+	]
+};
