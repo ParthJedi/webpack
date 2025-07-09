57b9a87deaa44dc0b8cb41ac6b108a9ebffa862a
Parth Joshi <parth.00.joshi@gmail.com>
2025-07-09 17:58:41 +0530

    Project production build after tree shaking.

--- webpack configured website/src/libs/showDate.js
+++ webpack configured website/src/libs/showDate.js
@@ -1,3 +1,7 @@
 export function showDate() {
 	console.log(mmnt(new Date()).format('DD-MM-YYYY'));
 }
+
+export function dummyFunc() {
+	console.log('Just a dummy function to demonstrate tree shaking.');
+}
